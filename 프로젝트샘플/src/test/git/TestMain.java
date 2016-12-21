928ab3a039c52acf57ceb8116e081a69c3f5e3bb
user2 <user2@KH2>
2016-12-21 16:44:17 +0900

    12/21/4:44

--- 프로젝트샘플/src/test/git/TestMain.java
+++ 프로젝트샘플/src/test/git/TestMain.java
@@ -2,9 +2,9 @@ package test.git;
 
 public class TestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		// 수정완료
+		int a;
 	}
 
 }
