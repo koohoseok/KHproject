d9a3850f71aeca323e88d1fced3e3ddb252befcd
user2 <user2@KH2>
2016-12-21 17:00:09 +0900

    수정

--- 프로젝트샘플/src/test/git/TestMain.java
+++ 프로젝트샘플/src/test/git/TestMain.java
@@ -3,12 +3,13 @@ package test.git;
 public class TestMain {
 
 	public static void main(String[] args) {		
-		// 수정완료
+		// �닔�젙�셿猷�
 		int a;
 		int b;
 		int c;
 		int D;
-		int e!; //느낌표는 사용할 수 없습니다
+		int e!; 
+		System.out.println("hi");
 	}
 
 }
